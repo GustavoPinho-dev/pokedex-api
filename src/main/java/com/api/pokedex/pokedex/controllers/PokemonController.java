cf6b4793315935a2055e1bde06308a6fc4f47cb0
godh <gustavo.rezende@gft.com>
2023-06-24 20:10:31 -0300

    Request getPokemons

--- src/main/java/com/api/pokedex/pokedex/controllers/PokemonController.java
+++ src/main/java/com/api/pokedex/pokedex/controllers/PokemonController.java
@@ -5,10 +5,12 @@ import com.api.pokedex.pokedex.models.PokemonData;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/pokemon")
+@RequestMapping("/pokemons")
 public class PokemonController {
 
     @GetMapping("/{name}")
@@ -20,5 +22,23 @@ public class PokemonController {
             // Tratar o erro adequadamente
             return null;
         }
+    }
+
+    @GetMapping
+    public List<PokemonData> getPokemons() {
+        List<PokemonData> pokemons = new ArrayList<>();
+
+        try {
+            for (int i = 1; i <= 150; i++) {
+                PokemonData pokemonData = PokemonAPI.getPokemonData(String.valueOf(i));
+                if (pokemonData != null) {
+                    pokemons.add(pokemonData);
+                }
+            }
+        } catch (IOException e) {
+            // Tratar o erro adequadamente
+        }
+
+        return pokemons;
     }
 }
